971796ca20312017e9bc77b63a7779e4c88271f6
tiffanydinhh <tiffanydinhh@gmail.com>
2025-10-16 22:00:51 -0700

    Added menu page structure

---
+++ bytes/src/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Menu() {
+
+    return(
+        <header>
+            <h1> Menu </h1>
+            <hr></hr>
+
+            <body> 
+                <p> introduction text here </p> 
+            </body>
+
+
+
+        </header>
+        
+
+
+    );
+}
+
+export default Menu;
