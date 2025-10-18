62febf596dad8f66f383096d39e7d59ea7b61fb6
tiffanydinhh <tiffanydinhh@gmail.com>
2025-10-17 23:28:52 -0700

    Started CSS for menu page

--- bytes/src/Menu.js
+++ bytes/src/Menu.js
@@ -1,22 +1,27 @@
 import React from "react";
+import "./Menu.css";
 
 function Menu() {
 
     return(
-        <header>
-            <h1> Menu </h1>
-            <hr></hr>
+        <div className = "menu-page"> 
+            <header className = "title">
+                <h1> MENU </h1>
+                <hr className = "divider"/>
+            </header>
 
-            <body> 
-                <p> introduction text here </p> 
-            </body>
+            <p className = "intro-text"> Welcome to our resturant menu! <br></br> Here you can explore all the types of dishes we offer.</p>
+            
+            <h3 className = "section-header"> APPETIZERS </h3>
 
+            <h3 className = "section-header"> MAIN ENTRÉES </h3>
 
+            <h3 className = "section-header"> SWEET TREATS </h3>
 
-        </header>
-        
+            <h3 className = "section-header"> DRINKS </h3>
+            
 
-
+        </div>
     );
 }
 
