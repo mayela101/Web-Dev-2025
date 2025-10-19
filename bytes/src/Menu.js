9db37cfa4dda7dc5eff2e518ab5999ba6b036dd7
tiffanydinhh <tiffanydinhh@gmail.com>
2025-10-19 00:07:08 -0700

    Added anchors

--- bytes/src/Menu.js
+++ bytes/src/Menu.js
@@ -6,19 +6,26 @@ function Menu() {
     return(
         <div className = "menu-page"> 
             <header className = "title">
-                <h1> MENU </h1>
+                <h1 className = "title"> MENU </h1>
                 <hr className = "divider"/>
             </header>
 
             <p className = "intro-text"> Welcome to our resturant menu! <br></br> Here you can explore all the types of dishes we offer.</p>
-            
-            <h3 className = "section-header"> APPETIZERS </h3>
 
-            <h3 className = "section-header"> MAIN ENTRÉES </h3>
+            <a href = "appetizers" className = "appetizer-anchor"> APPETIZERS</a>
+            <a href = "main entrées" className = "main-entrées-anchor"> MAIN ENTRÉES</a>
+            <a href = "sweet treats" className = "sweet-treats-anchor"> SWEET TREATS</a>
+            <a href = "drinks" className = "drinks-anchor"> DRINKS</a>
 
-            <h3 className = "section-header"> SWEET TREATS </h3>
+            <hr className = "divider"/>
 
-            <h3 className = "section-header"> DRINKS </h3>
+            <h3 id = "appetizers" className = "section-header"> APPETIZERS </h3>
+
+            <h3 id = "main entrées" className = "section-header"> MAIN ENTRÉES </h3>
+
+            <h3 id = "sweet treats" className = "section-header"> SWEET TREATS </h3>
+
+            <h3 id = "drinks" className = "section-header"> DRINKS </h3>
             
 
         </div>
