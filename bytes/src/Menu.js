37c50e45e7eb0e5510848b42016ab042547c64c2
tiffanydinhh <tiffanydinhh@gmail.com>
2025-10-20 21:33:26 -0700

    Fixed anchors

--- bytes/src/Menu.js
+++ bytes/src/Menu.js
@@ -12,18 +12,18 @@ function Menu() {
 
             <p className = "intro-text"> Welcome to our resturant menu! <br></br> Here you can explore all the types of dishes we offer.</p>
 
-            <a href = "appetizers">APPETIZERS</a>
-            <a href = "main entrées">MAIN ENTRÉES</a>
-            <a href = "sweet treats">SWEET TREATS</a>
-            <a href = "drinks">DRINKS</a>
+            <a href = "#appetizers">APPETIZERS</a>
+            <a href = "#main-entrees">MAIN ENTRÉES</a>
+            <a href = "#sweet-treats">SWEET TREATS</a>
+            <a href = "#drinks">DRINKS</a>
 
             <hr className = "divider"/>
 
             <h3 id = "appetizers" className = "section-header"> APPETIZERS </h3>
 
-            <h3 id = "main entrées" className = "section-header"> MAIN ENTRÉES </h3>
+            <h3 id = "main-entrees" className = "section-header"> MAIN ENTRÉES </h3>
 
-            <h3 id = "sweet treats" className = "section-header"> SWEET TREATS </h3>
+            <h3 id = "sweet-treats" className = "section-header"> SWEET TREATS </h3>
 
             <h3 id = "drinks" className = "section-header"> DRINKS </h3>
             
