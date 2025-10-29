3e364e43ea5b174ff9cec14cf207af801fd0e788
Sara Downing <downingsara05@gmail.com>
2025-10-29 14:56:07 -0700

    Added more content to footer

--- bytes/src/Footer.js
+++ bytes/src/Footer.js
@@ -7,12 +7,19 @@ import "./App.css";
 export default function Footer() {
   return(
     <footer className="footer">
-      <div className="social-media-icons">
-        <FaInstagram />
-        <FaFacebook />
-        <FaYoutube />
+      <div className="footer-left">
+        <p>1234 Bytes Avenue, Pomona, CA 91768</p>
+        <p>(123) 456-7890</p>
+        <p>catering@bytescafe.com</p>
       </div>
-      <p>©2025 Bytes™ All rights reserved.</p>
+      <div className="footer-right">
+        <div className="social-media-icons">
+            <FaInstagram />
+            <FaFacebook />
+            <FaYoutube />
+        </div>
+        <p>©2025 Bytes™ All rights reserved.</p>
+      </div>
     </footer>
     )
 }
