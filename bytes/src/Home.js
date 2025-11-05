e1fc0d7d814d839cf4ea53b4330af713ce940ef2
mayela101 <mancheta@cpp.edu>
2025-11-04 22:23:14 -0800

    Menu Image Integration + Home page fixes

--- bytes/src/Home.js
+++ bytes/src/Home.js
@@ -2,31 +2,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
+
+// local images (adjust paths if your file is in a different folder)
 import homeImage from "./assets/home_img1.png";
 import homeImage2 from "./assets/home_img2.png";
 import homeImage3 from "./assets/home_img3.png";
 import homeImage4 from "./assets/home_img4.png";
 
-
 // Content Block Component
-function ContentBlock({ image, title, description, alt, isAlternate, onClick }) {
+function ContentBlock({ block, isAlternate, onClick }) {
   return (
-    <div 
-      className={`content-block ${isAlternate ? 'alternate' : ''}`}
+    <div
+      className={`content-block block-${block.id} ${isAlternate ? "alternate" : ""}`}
       onClick={onClick}
-      style={{ cursor: 'pointer' }}
+      style={{ cursor: "pointer" }}
     >
       <div className="content-block-image">
-        <img src={image} alt={alt} />
+        <img src={block.image} alt={block.alt || ""} />
       </div>
       <div className="content-block-text">
-        <h3 className="content-block-title">{title}</h3>
-        <p className="content-block-description">{description}</p>
+        {block.title && <h3 className="content-block-title">{block.title}</h3>}
+        {block.description && (
+          <p className="content-block-description">{block.description}</p>
+        )}
       </div>
     </div>
   );
 }
-
 
 export default function Home() {
   const navigate = useNavigate();
@@ -35,53 +37,50 @@ export default function Home() {
     {
       id: 1,
       image: homeImage,
-      title: "About Bytes",
-      description: "This is a sample description for the menu content block XDDDD. The images are just placeholders for now feel free to change them later.",
-      alt: "About Bytes"
+      title: "ABOUT BYTES",
+      description:
+        "This is a sample description for the menu content block XDDDD. The images are just placeholders for now feel free to change them later.",
+      alt: "About Bytes",
     },
     {
       id: 2,
       image: homeImage2,
-      title: "Did you know?",
-      description: "This is a another sample description for the idk we'll just add a fictional fun fact about the restaurant or something XDDDD",
-      alt: "Did you know?"
+      title: "DID YOU KNOW?",
+      description: "Another sample description here.",
+      alt: "Did you know?",
     },
     {
       id: 3,
       image: homeImage3,
-      title: "Meet the Owners",
-      description: "Another sample description, idk we can insert a pic of us and pretend we're founders",
-      alt: "Menu Item #3"
+      title: "MEET THE OWNERS",
+      description:
+        "Another sample description, idk we can insert a pic of us and pretend we're founders",
+      alt: "Meet the Owners",
     },
     {
       id: 4,
       image: homeImage4,
-      title: "Fun Fact",
+      title: "FUN FACT",
       description: "Hiiii",
-      alt: "Fun Fact"
-    }
+      alt: "Fun Fact",
+    },
   ];
 
   const handleContentBlockClick = () => {
-    navigate('/Menu');
+    navigate("/menu"); // make sure your route is lowercase /menu
   };
 
   return (
     <main className="home">
+      {/* Banner area */}
+      <div className="banner" />
 
-
-      <div className="banner">
-      </div>
-
-      
+      {/* Content blocks */}
       <div className="content-blocks-container">
         {contentBlocks.map((block, index) => (
           <ContentBlock
             key={block.id}
-            image={block.image}
-            title={block.title}
-            description={block.description}
-            alt={block.alt}
+            block={block}
             isAlternate={index % 2 === 1}
             onClick={handleContentBlockClick}
           />
