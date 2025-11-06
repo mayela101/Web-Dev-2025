168d0573fbf8ab6a693cdb5f19d2689110086537
whyoho <amw1388@gmail.com>
2025-11-05 18:21:01 -0800

    More cats

--- bytes/src/components/Navbar.jsx
+++ bytes/src/components/Navbar.jsx
@@ -2,7 +2,8 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
-import bg from "../assets/nav-bg.png";
+import bg from "../assets/temporary_cat_nav.jpg";
+import kitten from "../assets/kitten.png";
 
 export default function Navbar() {
   return (
@@ -23,7 +24,12 @@ export default function Navbar() {
             <NavLink to="/menu"    className={styles.link}>MENU</NavLink>
           </nav>
 
-          <div className={styles.right}>{/* icons later */}</div>
+          <div className={styles.right}>
+            {/* testing logo linking */}
+            <NavLink to="/about"  className={styles.link}>
+              <img src={kitten} alt="a kitten" width="60px" height="40px"/>
+            </NavLink>
+          </div>
         </div>
       </div>
     </header>
